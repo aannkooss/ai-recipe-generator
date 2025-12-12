baf9931f865f963a13ee16e98da11ec25f88230d
Ankush Joshi <joshiankush6@gmail.com>
2025-12-11 23:14:04 -0800

    Fixing app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,165 @@
-import { useState } from "react";
-import type { FormEvent } from "react";
+// import { useState } from "react";
+// import type { FormEvent } from "react";
+// import "./App.css";
+// import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "../amplify/data/resource";
+// import { Amplify } from "aws-amplify";
+// import outputs from "../amplify_outputs.json";
+
+// Amplify.configure(outputs);
+
+// const client = generateClient<Schema>();
+
+// function App() {
+//   const [recipe, setRecipe] = useState<string | null>(null);
+//   const [loading, setLoading] = useState(false);
+
+//   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     try {
+//       setLoading(true);
+//       const { data, errors } = await client.queries.askBedrock({
+//         ingredients: (
+//           event.currentTarget.elements.namedItem("ingredients") as HTMLInputElement
+//         ).value.split(',').map(s => s.trim()),
+//       });
+//       if (errors) {
+//         console.error(errors);
+//         return;
+//       }
+//       console.log(data);
+//       setRecipe(data?.body ?? null);
+//     } catch (e) {
+//       console.error(e);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <main>
+//       <div className="app-container">
+//         <div className="header-container">
+//           <h1 className="main-header">
+//             Meet Your Personal
+//             <br />
+//             <span className="highlight">Recipe AI</span>
+//           </h1>
+//           <p className="description">
+//             Simply type a few ingredients using the format ingredient1, ingredient2,
+//             etc., and Recipe AI will generate an all-new recipe on demand...
+//           </p>
+//         </div>
+//         <form onSubmit={onSubmit} className="form-container">
+//           <div className="search-container">
+//             <input
+//               type="text"
+//               className="wide-input"
+//               id="ingredients"
+//               name="ingredients"
+//               placeholder="Ingredient1, Ingredient2, Ingredient3, ... etc"
+//             />
+//             <button type="submit" className="search-button" disabled={loading}>
+//               {loading ? "Generating..." : "Generate Recipe"}
+//             </button>
+//           </div>
+//         </form>
+//         {recipe && (
+//           <div className="result-container">
+//             <h2>Generated Recipe</h2>
+//             <p className="result">{recipe}</p>
+//           </div>
+//         )}
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import { FormEvent, useState } from "react";
+import { Loader, Placeholder } from "@aws-amplify/ui-react";
 import "./App.css";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../amplify/data/resource";
 import { Amplify } from "aws-amplify";
+import { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
+
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+const amplifyClient = generateClient<Schema>({
+ authMode: "userPool",
+});
 
 function App() {
-  const [recipe, setRecipe] = useState<string | null>(null);
+  const [result, setResult] = useState<string>("");
   const [loading, setLoading] = useState(false);
-
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    try {
-      setLoading(true);
-      const { data, errors } = await client.queries.askBedrock({
-        ingredients: (
-          event.currentTarget.elements.namedItem("ingredients") as HTMLInputElement
-        ).value.split(',').map(s => s.trim()),
-      });
-      if (errors) {
-        console.error(errors);
-        return;
-      }
-      console.log(data);
-      setRecipe(data?.body ?? null);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setLoading(false);
-    }
-  };
+    setLoading(true);
+  try {
+    const formData = new FormData(event.currentTarget);
+    const { data, errors } = await amplifyClient.queries.askBedrock({
+      ingredients: [formData.get("ingredients")?.toString() || ""],
+    });
+ 
+  if (!errors) {
+    setResult(data?.body || "No data returned");
+  } else {
+    console.log(errors);
+  }
+ 
+  } catch (e) {
+    alert(`An error occurred: ${e}`);
+  } finally {
+    setLoading(false);
+  }
+};
 
-  return (
-    <main>
-      <div className="app-container">
-        <div className="header-container">
-          <h1 className="main-header">
-            Meet Your Personal
-            <br />
-            <span className="highlight">Recipe AI</span>
-          </h1>
-          <p className="description">
-            Simply type a few ingredients using the format ingredient1, ingredient2,
-            etc., and Recipe AI will generate an all-new recipe on demand...
-          </p>
-        </div>
-        <form onSubmit={onSubmit} className="form-container">
-          <div className="search-container">
-            <input
-              type="text"
-              className="wide-input"
-              id="ingredients"
-              name="ingredients"
-              placeholder="Ingredient1, Ingredient2, Ingredient3, ... etc"
-            />
-            <button type="submit" className="search-button" disabled={loading}>
-              {loading ? "Generating..." : "Generate Recipe"}
-            </button>
-          </div>
-        </form>
-        {recipe && (
-          <div className="result-container">
-            <h2>Generated Recipe</h2>
-            <p className="result">{recipe}</p>
-          </div>
-        )}
-      </div>
-    </main>
-  );
+ return (
+  <div className="app-container">
+    <div className="header-container">
+      <h1 className="main-header">
+      Meet Your Personal
+      <br />
+      <span className="highlight">Recipe AI</span>
+    </h1>
+    <p className="description">
+    Simply type a few ingredients using the format ingredient1,
+    ingredient2, etc., and Recipe AI will generate an all-new
+    recipe on demand...
+    </p>
+  </div>
+  <form onSubmit={onSubmit} className="form-container">
+    <div className="search-container">
+      <input
+        type="text"
+        className="wide-input"
+        id="ingredients"
+        placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
+        name="ingredients"
+      />
+      <button type="submit" className="search-button">
+        Generate
+      </button>
+    </div>
+  </form>
+  <div className="result-container">
+    {loading ? (
+      <div className="loader-container">
+        <p>Loading...</p>
+        <Loader size="large" />
+        <Placeholder size="large" />
+        <Placeholder size="large" />
+        <Placeholder size="large" />
+      </div> 
+    ) : (
+      result && <p className="result">{result}</p>
+    )}
+  </div>
+ </div>
+ );
 }
-
 export default App;
